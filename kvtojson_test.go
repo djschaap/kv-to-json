ef54211fe3c79441500e471f06368be402b2ca78
Doug Schaapveld <djschaap@users.noreply.github.com>
2020-08-25 10:00:35 -0500

    Improve testing

---
+++ kvtojson_test.go
@@ -0,0 +1,25 @@
+package kvtojson
+
+import (
+	"github.com/djschaap/logevent"
+	"github.com/djschaap/logevent/fromenv"
+	"testing"
+)
+
+func Test_New(t *testing.T) {
+	// env is a local object, but stores state GLOBALLY
+	env := fromenv.NewFakeEnv()
+	env.Setenv("SENDER_PACKAGE", "senddump")
+
+	var sender logevent.MessageSender
+	sender, err := fromenv.GetMessageSenderFromEnv()
+	if err != nil {
+		t.Errorf("expected success but got error: %s", err)
+	}
+
+	var sess *Sess
+	sess = New(sender)
+	if sess == nil {
+		t.Error("expected kvtojson session but got nil")
+	}
+}
